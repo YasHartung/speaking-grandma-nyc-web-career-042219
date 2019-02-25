eaef90ec4f532fac1945aff9634a90c25698b6e7
Yasmine Hartung <y.hartung@gmail.com>
2019-02-25 08:26:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,14 +5,11 @@ def speak_to_grandma (phrase)
   if (phrase != phrase.upcase)
     return "HUH?! SPEAK UP, SONNY!"
 
-<<<<<<< HEAD
   elsif (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
+    
   else
     return "NO, NOT SINCE 1938!"
   end
-=======
-  else return "I LOVE YOU TOO PUMPKIN!"
-
->>>>>>> ac4df87abfc31c0a0c6486b699ef7243275e905f
+  
 end
